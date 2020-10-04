e08e98dfb3757d072b0625f8ff6623dda123a7bc
mathuo <matthewoconnor@hotmail.co.uk>
2020-10-04 22:57:40 +0100

    code

---
+++ packages/splitview/src/__tests__/math.spec.ts
@@ -0,0 +1,11 @@
+import { clamp } from '../math';
+
+describe('math', () => {
+    describe('clamp', () => {
+        it('should clamp between a minimum and maximum value', () => {
+            expect(clamp(45, 40, 50)).toBe(50);
+            expect(clamp(35, 40, 50)).toBe(40);
+            expect(clamp(55, 40, 50)).toBe(50);
+        });
+    });
+});
