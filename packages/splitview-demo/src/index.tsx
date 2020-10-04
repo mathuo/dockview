96baff9d4354acefc7caa7ef35506094e83e0961
mathuo <matthewoconnor@hotmail.co.uk>
2020-10-04 22:55:37 +0100

    code

--- packages/splitview-demo/src/index.tsx
+++ packages/splitview-demo/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { App } from './app';
 import './index.scss';
+import { Application } from './layout-grid/application';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Application />, document.getElementById('app'));
