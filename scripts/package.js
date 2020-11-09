29442aae78bcf0ad68460d9ca809271959ffe43f
mathuo <matthewoconnor@hotmail.co.uk>
2020-11-09 21:56:50 +0000

    fix script

--- scripts/package.js
+++ scripts/package.js
@@ -6,5 +6,5 @@ const output = path.join(__dirname, "../output");
 const dir1 = path.join(__dirname, "../packages/splitview/typedocs");
 const dir2 = path.join(__dirname, "../packages/splitview-demo/dist");
 
-fs.copySync(dir1, path.join(output,'build'));
-fs.copySync(dir2, path.join(output,'docs'));
+fs.copySync(dir1, path.join(output,'docs'));
+fs.copySync(dir2, path.join(output,'build'));
