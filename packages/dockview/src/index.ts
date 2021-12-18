699a1fb8bb33e4d1961e8747f3d0c2e051b2237a
mathuo <6710312+mathuo@users.noreply.github.com>
2021-12-18 16:02:57 +0000

    feat: export additional types

--- packages/dockview/src/index.ts
+++ packages/dockview/src/index.ts
@@ -1,27 +1,34 @@
 import './footnote';
 
-export * from './splitview/core/splitview';
-export * from './splitview/splitviewComponent';
-export * from './paneview/paneview';
-export * from './paneview/paneviewComponent';
-export * from './gridview/gridview';
-export * from './gridview/baseComponentGridview';
-export * from './groupview/groupview';
-export * from './groupview/panel/content';
-export * from './groupview/tab';
 export * from './events';
 export * from './lifecycle';
-export * from './groupview/groupPanel';
-export * from './api/component.api';
-export * from './groupview/types';
-export * from './dockview/dockviewComponent';
-export * from './dockview/options';
-export * from './gridview/gridviewComponent';
 export * from './dnd/dataTransfer';
-export { Position } from './dnd/droptarget';
+export * from './api/component.api';
+
+export * from './splitview/core/splitview';
+export * from './paneview/paneview';
+export * from './gridview/gridview';
+export * from './groupview/groupview';
+export * from './gridview/baseComponentGridview';
+
+export * from './groupview/panel/content';
+export * from './groupview/tab';
+export * from './groupview/types';
+export * from './dockview/options';
+
+export * from './dockview/dockviewComponent';
+export * from './gridview/gridviewComponent';
+export * from './splitview/splitviewComponent';
+export * from './paneview/paneviewComponent';
+
+export * from './gridview/gridviewPanel';
+export * from './splitview/splitviewPanel';
+export * from './paneview/paneviewPanel';
+export * from './groupview/groupPanel';
 
 export * from './react'; // TODO: should be conditional on whether user wants the React wrappers
 
+export { Position } from './dnd/droptarget';
 export {
     StateObject,
     State,
