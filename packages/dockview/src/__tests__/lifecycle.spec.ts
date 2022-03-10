943ea3b93ed43e33189b66e2561356a4f55b60b3
mathuo <6710312+mathuo@users.noreply.github.com>
2022-03-10 20:58:23 +0000

    chore: add tests

--- packages/dockview/src/__tests__/lifecycle.spec.ts
+++ packages/dockview/src/__tests__/lifecycle.spec.ts
@@ -1,4 +1,4 @@
-import { MutableDisposable } from '../lifecycle';
+import { CompositeDisposable, MutableDisposable } from '../lifecycle';
 
 describe('lifecycle', () => {
     test('mutable disposable', () => {
@@ -22,5 +22,30 @@ describe('lifecycle', () => {
         expect(disposed).toBe(2);
 
         mutableDisposable.dispose();
+    });
+
+    test('composite disposable', () => {
+        const d1 = {
+            dispose: jest.fn(),
+        };
+        const d2 = {
+            dispose: jest.fn(),
+        };
+        const d3 = {
+            dispose: jest.fn(),
+        };
+        const d4 = {
+            dispose: jest.fn(),
+        };
+
+        const cut = new CompositeDisposable(d1, d2);
+        cut.addDisposables(d3, d4);
+
+        cut.dispose();
+
+        expect(d1.dispose).toHaveBeenCalledTimes(1);
+        expect(d2.dispose).toHaveBeenCalledTimes(1);
+        expect(d3.dispose).toHaveBeenCalledTimes(1);
+        expect(d4.dispose).toHaveBeenCalledTimes(1);
     });
 });
