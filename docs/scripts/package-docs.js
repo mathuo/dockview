364cb7ce27f78b77865b4e49eda3f98152246e07
mathuo <mathuo@users.noreply.github.com>
2022-05-22 03:39:08 +0000

    Deploying to gh-pages from  @ 2774a27eda3d0b9a57fcff748d60b0111bb52415 🚀

--- docs/scripts/package-docs.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs-extra');
-const path = require('path');
-
-const output = path.join(__dirname, '../../');
-
-const docsDir = path.join(__dirname, '../../docs/build');
-
-fs.copySync(docsDir, path.join(output, 'docs2'));
