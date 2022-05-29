c75964632b9535aaf6990576d265b10b037e65ef
mathuo <6710312+mathuo@users.noreply.github.com>
2022-05-29 16:21:20 +0100

    chore: improve docs

--- packages/docs/src/components/dockview/contextMenu.tsx
+++ packages/docs/src/components/dockview/contextMenu.tsx
@@ -3,65 +3,59 @@ import {
     DockviewReadyEvent,
     IDockviewPanelHeaderProps,
     IDockviewPanelProps,
+    TabContextMenuEvent,
 } from 'dockview';
 import * as React from 'react';
+import './contextMenu.scss';
 
-//
 const components = {
     default: (props: IDockviewPanelProps<{ title: string }>) => {
         return <div style={{ padding: '20px' }}>{props.params.title}</div>;
     },
 };
 
-const DefaultTab = (props: IDockviewPanelHeaderProps) => {
-    const [active, setActive] = React.useState<boolean>(props.api.isActive);
-    const [groupActive, setGroupActive] = React.useState<boolean>(
-        props.api.isGroupActive
+const CustomTab = (
+    props: IDockviewPanelHeaderProps & React.DOMAttributes<HTMLDivElement>
+) => {
+    const onClose = React.useCallback(
+        (event: React.MouseEvent<HTMLSpanElement>) => {
+            event.stopPropagation();
+            props.api.close();
+        },
+        [props.api]
     );
 
-    React.useEffect(() => {
-        const disposable1 = props.api.onDidActiveChange((e) => {
-            setActive(e.isActive);
-        });
-        const disposable2 = props.containerApi.onDidActiveGroupChange((e) => {
-            setGroupActive(props.api.isGroupActive);
-        });
+    const onClick = React.useCallback(
+        (event: React.MouseEvent<HTMLDivElement>) => {
+            props.api.setActive();
 
-        return () => {
-            disposable1.dispose();
-            disposable2.dispose();
-        };
-    }, [props.api]);
+            if (props.onClick) {
+                props.onClick(event);
+            }
+        },
+        [props.api, props.onClick]
+    );
 
     return (
-        <div
-            style={{
-                display: 'flex',
-                padding: '0px 8px',
-                alignItems: 'center',
-                height: '100%',
-            }}
-        >
-            <span style={{ padding: '0px 8px', flexGrow: 1 }}>
-                {props.api.title}
-            </span>
-            <span
-                className=""
-                onClick={() => props.api.setActive()}
-                style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    paddingRight: '8px',
-                }}
-            >
+        <div {...props} onClick={onClick} className="dockview-react-tab">
+            <span className="dockview-react-tab-title">{props.api.title}</span>
+            <span onClick={onClose} className="dockview-react-tab-action">
                 {'✕'}
             </span>
         </div>
     );
 };
 
+const Test = (props: IDockviewPanelHeaderProps) => {
+    const onContextMenu = (event: React.MouseEvent) => {
+        event.preventDefault();
+        alert('hiya');
+    };
+    return <CustomTab onContextMenu={onContextMenu} {...props} />;
+};
+
 const tabComponents = {
-    default: DefaultTab,
+    default: Test,
 };
 
 export const ContextMenuDockview = () => {
