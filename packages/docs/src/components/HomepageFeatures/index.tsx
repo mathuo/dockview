1865fe7ca0bb870597514e2422bd74135a9eb9c8
mathuo <6710312+mathuo@users.noreply.github.com>
2022-05-30 22:00:44 +0100

    chore: docs

--- packages/docs/src/components/HomepageFeatures/index.tsx
+++ packages/docs/src/components/HomepageFeatures/index.tsx
@@ -3,68 +3,112 @@ import clsx from 'clsx';
 import styles from './styles.module.css';
 
 type FeatureItem = {
-  title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
-  description: JSX.Element;
+    title: string;
+    Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+    description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+    {
+        title: '',
+        Svg: require('@site/static/img/dockview_grid_2.svg').default,
+        description: (
+            <>
+                <div className="feature-banner">
+                    <h3 className="feature-banner-header">
+                        Serialziable layouts
+                    </h3>
+                    <p className="feature-banner-content">
+                        Add and remove panels using the provided api or use our
+                        serialization method to persist layouts
+                    </p>
+                </div>
+                <div className="feature-banner">
+                    <h3 className="feature-banner-header">
+                        Customizable Theme
+                    </h3>
+                    <p className="feature-banner-content">
+                        Overide a number of provided CSS Properties for a simple
+                        change or target specific classes for a more customized
+                        approach
+                    </p>
+                </div>
+                <div className="feature-banner">
+                    <h3 className="feature-banner-header">
+                        Variety of Controls
+                    </h3>
+                    <p className="feature-banner-content">
+                        Whether you want a simple splitview or a complete
+                        docking solution dockview has you covered
+                    </p>
+                </div>
+            </>
+        ),
+    },
+    {
+        title: '',
+        Svg: require('@site/static/img/dockview_splash_2.svg').default,
+        description: (
+            <>
+                <div className="feature-banner">
+                    <h3 className="feature-banner-header">Drag and Drop</h3>
+                    <p className="feature-banner-content">
+                        Supports a variety of drag and drop functionailties as
+                        well as providing the developer with an ability to
+                        interact with drag events
+                    </p>
+                </div>
+                <div className="feature-banner">
+                    <h3 className="feature-banner-header">Zero Dependencies</h3>
+                    <p className="feature-banner-content">
+                        Zero dependencies, that's all
+                    </p>
+                </div>
+                <div className="feature-banner">
+                    <h3 className="feature-banner-header">
+                        Code Quality and Transparency
+                    </h3>
+                    <p className="feature-banner-content">
+                        All our code is run through Sonarcloud Code Analyis
+                        which along with our source code and high test coverage
+                        can be viewed from our Github page
+                    </p>
+                </div>
+            </>
+        ),
+    },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+function Feature({ title, Svg, description }: FeatureItem) {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                margin: 'auto',
+                justifyContent: 'center',
+            }}
+            className="dockview-feature"
+        >
+            <Svg className={styles.featureSvg} role="img" />
+            <div style={{ maxWidth: '400px', paddingLeft: '20px' }}>
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
 }
 
 export default function HomepageFeatures(): JSX.Element {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <div className="row">
+                    {FeatureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
