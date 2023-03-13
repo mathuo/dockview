d60b4c6ba2d49cdc44d60024a95238225faab197
mathuo <6710312+mathuo@users.noreply.github.com>
2023-03-13 18:44:57 +0300

    tests: add tests

---
+++ packages/dockview/src/__tests__/dockview/dockview.spec.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import { DockviewApi } from 'dockview-core';
+import {
+    IDockviewPanelProps,
+    DockviewReact,
+    DockviewReadyEvent,
+} from '../../dockview/dockview';
+import { PanelCollection } from '../../types';
+import { setMockRefElement } from '../__test_utils__/utils';
+
+describe('gridview react', () => {
+    let components: PanelCollection<IDockviewPanelProps>;
+
+    beforeEach(() => {
+        components = {
+            default: (props: IDockviewPanelProps) => {
+                return <div>hello world</div>;
+            },
+        };
+    });
+
+    test('default', () => {
+        let api: DockviewApi | undefined;
+
+        const onReady = (event: DockviewReadyEvent) => {
+            api = event.api;
+        };
+
+        render(<DockviewReact components={components} onReady={onReady} />);
+
+        expect(api).toBeTruthy();
+    });
+
+    test('is sized to container', () => {
+        setMockRefElement({
+            clientHeight: 450,
+            clientWidth: 650,
+            appendChild: jest.fn(),
+        });
+        let api: DockviewApi | undefined;
+
+        const onReady = (event: DockviewReadyEvent) => {
+            api = event.api;
+        };
+
+        render(<DockviewReact components={components} onReady={onReady} />);
+
+        expect(api.width).toBe(650);
+        expect(api.height).toBe(450);
+    });
+});
