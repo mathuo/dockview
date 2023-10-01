e71fd7b15325fda597327a94f9d8d0777f39bf42
mathuo <6710312+mathuo@users.noreply.github.com>
2023-10-01 20:44:26 +0100

    chore: auto-gen type docs

--- packages/docs/src/pages/index.tsx
+++ packages/docs/src/pages/index.tsx
@@ -3,35 +3,10 @@ import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import DockviewDemo from '@site/sandboxes/demo-dockview/src/app';
-import DockviewDemo2 from '@site/sandboxes/dockview-app/src/app';
-import { MultiFrameworkContainer } from '../components/ui/container';
-import { BrowserHeader } from '../components/ui/browserHeader';
-import './index.scss';
 import { Introduction } from '../components/HomepageFeatures/introduction';
-
-function HomepageHeader() {
-    const { siteConfig } = useDocusaurusContext();
-    return (
-        <header className={clsx('hero hero--primary', styles.heroBanner)}>
-            <div className="container">
-                <h1 className="hero__title">{siteConfig.title}</h1>
-                <p className="hero__subtitle">{siteConfig.tagline}</p>
-                <div className={styles.buttons}>
-                    <Link
-                        className="button button--secondary button--lg"
-                        to={useBaseUrl('docs/')}
-                    >
-                        Get Started
-                    </Link>
-                </div>
-            </div>
-        </header>
-    );
-}
+import styles from './index.module.css';
+import './index.scss';
 
 function HomepageHeader2() {
     const { siteConfig } = useDocusaurusContext();
@@ -74,6 +49,7 @@ export default function Home(): JSX.Element {
             description={`${siteConfig.description}`}
         >
             <HomepageHeader2 />
+
             <main className="container">
                 <Introduction />
             </main>
