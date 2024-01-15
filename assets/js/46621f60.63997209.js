0ba0384a7200e512928e7c6eb38098662358739c
mathuo <mathuo@users.noreply.github.com>
2024-01-15 20:29:32 +0000

    Deploying to gh-pages from  @ 8a67f60747d4e38db278eb4fda918db1b121ae8e 🚀

---
+++ assets/js/46621f60.63997209.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdockview_docs=self.webpackChunkdockview_docs||[]).push([[8323],{1707:e=>{e.exports=JSON.parse('{"title":"Components","slug":"/category/components","permalink":"/docs/1.8.4/category/components","navigation":{"previous":{"title":"Theme","permalink":"/docs/1.8.4/theme"},"next":{"title":"Dockview","permalink":"/docs/1.8.4/components/dockview"}}}')}}]);
