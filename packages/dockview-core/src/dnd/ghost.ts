6d2f2f1012dd889da08416b0a40c604e5779907b
mathuo <6710312+mathuo@users.noreply.github.com>
2025-04-29 21:44:55 +0100

    bug: fix ghost image rendering

--- packages/dockview-core/src/dnd/ghost.ts
+++ packages/dockview-core/src/dnd/ghost.ts
@@ -8,6 +8,9 @@ export function addGhostImage(
     // class dockview provides to force ghost image to be drawn on a different layer and prevent weird rendering issues
     addClasses(ghostElement, 'dv-dragged');
 
+    // move the element off-screen initially otherwise it may in some cases be rendered at (0,0) momentarily
+    ghostElement.style.top = '-9999px';
+
     document.body.appendChild(ghostElement);
     dataTransfer.setDragImage(ghostElement, options?.x ?? 0, options?.y ?? 0);
 
