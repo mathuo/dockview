48bd059d76a17a082b1d7c4dd4676d6384894123
mathuo <mathuo@users.noreply.github.com>
2025-07-20 19:30:43 +0000

    Deploying to gh-pages from  @ 5c6f6d203e5d1ef5e55cff721f04d71425101375 🚀

--- templates/dockview/update-title/typescript/src/index.ts
+++ templates/dockview/update-title/typescript/src/index.ts
@@ -6,6 +6,7 @@ import {
     ITabRenderer,
     PanelUpdateEvent,
     Parameters,
+    themeAbyss,
 } from 'dockview-core';
 
 class Panel implements IContentRenderer {
@@ -50,7 +51,7 @@ class Panel implements IContentRenderer {
 }
 
 const api = createDockview(document.getElementById('app'), {
-    className: 'dockview-theme-abyss',
+    theme: themeAbyss,
     createComponent: (options) => {
         switch (options.name) {
             case 'default':
